2fdd59911309b53b99914a9e95ef2c7d1622f7a8
Adrian Knoth <adi@drcomp.erfurt.thur.de>
2016-02-12 12:39:26 +0100

    Imported Upstream version 3.1.8

--- etc/spotlight/sparql_parser.c
+++ etc/spotlight/sparql_parser.c
@@ -123,8 +123,8 @@
 
 /* In a future release of Bison, this section will be replaced
    by #include "y.tab.h".  */
-#ifndef YY_YY_Y_TAB_H_INCLUDED
-# define YY_YY_Y_TAB_H_INCLUDED
+#ifndef YY_YY_SPARQL_PARSER_H_INCLUDED
+# define YY_YY_SPARQL_PARSER_H_INCLUDED
 /* Enabling traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
@@ -220,7 +220,7 @@ int yyparse ();
 /* Line 387 of yacc.c  */
 #line 222 "sparql_parser.c"
 
-#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
+#endif /* !YY_YY_SPARQL_PARSER_H_INCLUDED  */
 
 /* Copy the second part of user declarations.  */
 
